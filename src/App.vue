7bec69b97c9e0d0c72a5e89de040fa3759d84420
Edvin Nordin <nordin.edvin@gmail.com>
2025-10-04 02:00:46 +0200

    color scheme

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { ref, nextTick } from 'vue'
-//import axios from 'axios'
 import { Ollama } from 'ollama'
 import { marked } from 'marked'
 
-const ollama = new Ollama({ host: 'http://192.168.68.105:11434' })
+const ollama = new Ollama({ host: 'http://192.168.68.105:11434' }) //http://localhost:11434
 const input = ref('')
 
 interface Message {
@@ -40,7 +39,7 @@ async function scrollDown() {
 </script>
 
 <template>
-  <div class="bg-gray-700 text-white mx-auto max-w-250">
+  <div class="text-white mx-auto max-w-250">
     <div class="mx-2 mb-15">
       <div
         v-for="(response, i) in responseHistory"
@@ -49,21 +48,21 @@ async function scrollDown() {
         :class="response.isUser ? 'justify-end' : 'justify-start'"
       >
         <div
-          class="inline-block p-3 rounded-xl text-xl break-words max-w-150"
-          :class="response.isUser ? 'bg-violet-400 text-white' : 'bg-gray-300 text-black'"
+          class="inline-block p-3 rounded-sm text-xl break-words max-w-150"
+          :class="response.isUser ? 'bg-violet-300 text-black' : 'bg-violet-100 text-black'"
           v-html="response.text"
         ></div>
       </div>
     </div>
     <div
-      class="fixed bottom-0 bg-gray-700 max-w-200 w-full left-0 right-0 mx-auto mb-1.5 rounded-md"
+      class="fixed bottom-0 max-w-200 w-full left-0 right-0 mx-auto mb-1.5 rounded-md bg-slate-600"
     >
-      <input
+      <textarea
         v-model="input"
-        @keyup.enter="newResponse"
-        placeholder="Ask anything"
-        class="rounded-md p-2 border-2 w-full focus:outline-hidden"
-      />
+        @keyup.shift.enter="newResponse"
+        placeholder="Press shift + enter to send your message"
+        class="rounded-sm p-2 border-2 w-full focus:outline-hidden h-11"
+      ></textarea>
     </div>
   </div>
 </template>
